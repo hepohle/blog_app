78985f0a75f32fe573eacbfa0efe315139a23a40
Hector Pohle <hectorpohle@gmail.com>
2020-12-22 22:36:17 -0300

    javascript

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  post_id    :integer          not null
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
